0465f50e94fa5fe46b3490f75eee37a5264e41c1
Kelly Ye <kellyye@Kellys-MacBook-Pro-3.local>
2022-11-23 17:39:41 -0800

    started the proj

--- solver.py
+++ solver.py
@@ -20,3 +20,36 @@ def solve(G):
     Assign a team to v with G.nodes[v]['team'] = team_id
     Access the team of v with team_id = G.nodes[v]['team']
     '''
+    feasible_k = #upperbound on k
+    for i in range(feasible_k):
+
+        #random assignment of teams
+        vertices = G.nodes
+        random.shuffle(vertices)
+
+        partitions = [Partition() for _ in range(i)]
+
+        curr_team = 0
+        for vertex in vertices:
+            G.nodes[vertex]['team'] = partitions[curr_team]
+            curr_team += 1
+            if curr_team == i:
+                curr_team = 0
+        
+        vertices_copy = vertices
+        #stimulated annealing
+        swaps = {} 
+        lowest_cost = #cost of v1 staying in this team
+        for v1 in vertices_copy:
+            other_vertices = vertices_copy.remove(v1)
+            for v2 in other_vertices:
+                if G.nodes[v1]['team'] != G.nodes[v2]['team']:
+
+
+                    #confused what compute_new_partition_cost() does
+                    if compute_new_partition_cost() ##cost of this swap < lowest_cost:
+                        swaps[v1] = G.nodes[v2]['team']
+                        
+
+
+        
